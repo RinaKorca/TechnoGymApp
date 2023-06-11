a787815984bfec5397480c48e644738f903962f0
RinaKorca <73649582+RinaKorca@users.noreply.github.com>
2023-06-11 14:22:34 +0200

    Create Header adn and add cart and search icon

--- frontend/Main.jsx
+++ frontend/Main.jsx
@@ -1,6 +1,5 @@
-import { View, Text , Safe, SafeAreaView, Platform} from 'react-native'
+
 import React from 'react'
-import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from './screens/Home'
@@ -10,10 +9,20 @@ const Stack = createNativeStackNavigator()
 const Main = () => {
   return (
 <NavigationContainer>
-<Stack.Navigator>
+<Stack.Navigator
+initialRouteName="home"
+screenOptions={{
+  headerShown: false,
+}}
+>
+
     <Stack.Group>
 
-<Stack.Screen name="home" component={Home}/>
+<Stack.Screen name="home"
+ component={Home}
+
+/>
+
 
     </Stack.Group>
 </Stack.Navigator>
--- frontend/screens/Home.jsx
+++ frontend/screens/Home.jsx
@@ -1,11 +1,79 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import {defaultStyle, colors} from "../styles/styles"
+import Header from '../components/Header'
+import { Avatar, Button } from 'react-native-paper'
 
 const Home = () => {
+
+
+  
   return (
+    <View style =
+    {defaultStyle}>
+    <Header/>
+
+    {/**Headin Row */}
+
+    <View 
+      style={{
+        paddingTop: 70,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}>
+
+         {/**Heading */}
+
     <View>
-      <Text>Home</Text>
+
+            <Text style={{fontSize: 25}}>Our</Text>
+            <Text style={{fontSize: 25, fontWeight: "900"}}>Products</Text>
+
     </View>
+
+        {/** Search Bar */}
+          <View>
+
+            <TouchableOpacity>
+              <Avatar.Icon 
+              icon={"magnify"} 
+              size={50}
+              color={"gray"} 
+              style={{backgroundColor: colors.color2, elevation: 12 }} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+
+        {/**Categories */}
+        <View
+          style={{
+            flexDirection: "row",
+            height: 80,
+          }}
+        >
+          
+
+        <Button
+          style={{
+            backgroundColor: colors.color5,
+            borderRadius: 100,
+            margin: 5
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 12,
+              color: "gray",
+            }}
+          >
+
+          </Text>
+          Nice
+        </Button>
+        </View>
+      </View>
   )
 }
 
