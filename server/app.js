8c1ab01ef05c1d907b3148608bc60904580ceca9
RinaKorca <73649582+RinaKorca@users.noreply.github.com>
2023-06-20 11:50:55 +0200

    Do backend authentication

--- server/app.js
+++ server/app.js
@@ -1,12 +1,28 @@
 import express from "express"
 import { config } from "dotenv"
+import cookieParser from "cookie-parser"
 
-config({
-    path: "./data/config.env",
+  config({
+      path: "./data/config.env",
+    })
+    
+  export const app = express()
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(
+    cors({
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      origin: [process.env.FRONTEND_URI_1, process.env.FRONTEND_URI_2],
+    })
+  );
+
+  app.get("/", (req, res, next) => {
+    res.send("Working");
   })
-  
-export const app = express()
-app.use(express.json())
+
+
+
 
 app.get("/", (req, res, next)=>{
     res.send("RINA")
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,8 @@
+import { asyncError } from "../middlewares/error.js"
 import { User } from "../models/user.js"
+import ErrorHandler from "../utils/error.js"
   
- export const login = async (req,res,next)=>{
+ export const login = asyncError(async (req,res,next)=>{
 
     const { email, password } = req.body
 
@@ -9,15 +11,21 @@ import { User } from "../models/user.js"
 
     const isMatched = await user.comparePassword(password)
 
-    if(!isMatched){
-        return res.status(400).json({success: false, message: "Incorrect password"})
-    }
+    if (!user) {
+        return next(new ErrorHandler("Incorrect Email or Password", 400));
+      }
+    
+      if (!password) return next(new ErrorHandler("Please Enter Password", 400));
+
+      if (!isMatched) {
+        return next(new ErrorHandler("Incorrect Email or Password", 400));
+      }
 
     res.status(200).json({
         success: true,
         message: `Welcome back, ${user.name}`,
     })
-}
+})
 
 export const signup = async(req,res,next)=>{
 
@@ -27,9 +35,14 @@ export const signup = async(req,res,next)=>{
     await User.create({
         name, email, password, address, city, country, pinCode,
     })
-    res.status(201).json({
-        success: true,
-        message: "Registered Successfully",
+    res
+    .status(200)
+    .cookie("token", token,{
+      expires: new Date(Date.now()+15*24*60*1000),
     })
+    .json({
+      success: true,
+      message: `Welcome Back, ${user.name}`,
+    });
 }
 
